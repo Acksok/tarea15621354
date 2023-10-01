0a9eb4ad8c8647c4b575483b8f5e995fba689990
Acksok <Alexisnowmichael@gmail.com>
2023-10-01 11:33:39 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-provider "github" {}
+provider "github" {
+GITHUB_TOKEN = "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"
+}
 
 # Retrieve information about the currently (PAT) authenticated user
 data "github_user" "self" {
